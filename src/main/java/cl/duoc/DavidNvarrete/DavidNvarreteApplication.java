79d02e7b04d30ed098f6668ef61bd2d7deda5135
David <dav.navarreteo@duocuc.cl>
2026-03-30 11:01:37 -0300

    PRIMER COMMIT

---
+++ src/main/java/cl/duoc/DavidNvarrete/DavidNvarreteApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.DavidNvarrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidNvarreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidNvarreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/DavidNvarrete/DavidNvarreteApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.DavidNvarrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavidNvarreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
